52a5e86eee10cd1d41ca31b4d653f7708df12b40
Vladimir <Vladigonzalez@gmail.com>
2017-07-28 22:41:04 -0400

    Clase23 Incorporar Bootstrap

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,24 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require 'js/jquery-1.11.1.min'
+//= require 'bootstrap/js/bootstrap.min'
+//= require 'js/jquery.backstretch.min'
+//= require 'js/wow.min'
+//= require 'js/retina-1.1.0.min'
+//= require 'js/waypoints.min'
+//= require 'js/jquery.magnific-popup.min'
+//= require 'js/scripts'
+
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
